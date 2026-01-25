e243bd96a6f025b7476143870855c313a858a08d
Justin Carlson <40642470+justcarlson@users.noreply.github.com>
2026-01-25 12:10:49 -0500

    chore(01-03): move static assets to WXT public directory
    - Copy extension icons (16px, 48px, 128px) to public/images/icons/
    - Copy i18n messages to public/_locales/en/messages.json
    - Add appName and appDescription keys for Chrome i18n
    - Update wxt.config.ts with icons and default_locale
    - Use __MSG_appName__ for i18n-aware manifest name

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "wxt";
 
 export default defineConfig({
 	manifest: {
-		name: "Zendesk Quicktab",
-		description: "Well behaved browser tabs for Zendesk agents",
+		name: "__MSG_appName__",
+		description: "__MSG_appDescription__",
 		permissions: ["webNavigation", "tabs", "scripting", "storage"],
 		host_permissions: ["http://*.zendesk.com/*", "https://*.zendesk.com/*"],
+		icons: {
+			16: "images/icons/icon-16.png",
+			48: "images/icons/icon-48.png",
+			128: "images/icons/icon-128.png",
+		},
+		default_locale: "en",
 	},
 	vite: () => ({
 		build: {
